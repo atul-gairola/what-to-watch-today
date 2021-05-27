c20472f7112d1b46aa17c86c5ba1b983e9156085
atul-gairola <atulgairola08@gmail.com>
2021-05-27 22:37:43 +0530

    Add endpoints file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-
-  
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://api.themoviedb.org/3/genre/movie/list?api_key=59c3960b67ab82984a2e267ccd195cc6"
+        );
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getData();
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">What do you wanna watch today ?</header>
+      <div>
+        <label for="show-radio">Show</label>
+        <input type="radio" name="typeOfContent" id="show-radio" value="show" />
+        <label for="movie-radio">Movie</label>
+        <input
+          type="radio"
+          name="typeOfContent"
+          id="movie-radio"
+          value="movie"
+          checked
+        />
+      </div>
     </div>
   );
 }
---
+++ src/endpoints.js
@@ -0,0 +1,7 @@
+export default endpoints = {
+  getMovieGenres:
+    "https://api.themoviedb.org/3/genre/tv/list?api_key=59c3960b67ab82984a2e267ccd195cc6&language=en-US",
+  getShowGenres:
+    "https://api.themoviedb.org/3/genre/tv/list?api_key=59c3960b67ab82984a2e267ccd195cc6&language=en-US",
+    
+};
