42516fd43b5d02ef4a7dbc7d4df3a96124105abe
atul-gairola <atulgairola08@gmail.com>
2021-05-30 12:01:27 +0530

    Add bibliography

---
+++ src/components/Bibliography.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Bibliography() {
+  return (
+    <div>
+      <h6>Icons</h6>
+      <p>
+        <a
+          target="_blank"
+          href="https://thenounproject.com/search/?q=show&i=3857978"
+        >
+          show
+        </a>{" "}
+        by priyanka from the Noun Project
+      </p>
+      <p>
+        <a
+          target="_blank"
+          href="https://thenounproject.com/search/?q=movie&i=1511635"
+        >
+          Movie
+        </a>{" "}
+        by AFY Studio from the Noun Project
+      </p>
+    </div>
+  );
+}
+
+export default Bibliography;
