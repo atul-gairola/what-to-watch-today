bb168095e198a3973854277173c0fc4500b3d102
atul-gairola <atulgairola08@gmail.com>
2021-05-30 14:32:51 +0530

    Add styling to layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
 
 import Nav from "../components/Nav";
 
+const useStyles = makeStyles({
+  container: {
+    padding: "50px",
+  },
+});
+
 function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Nav />
-      <div>{children}</div>
+      <div className={classes.container}>{children}</div>
     </div>
   );
 }
